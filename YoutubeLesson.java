7f41228e757795219cc3f961339cdd4ea2d1f94f
DanielOnuwka <152315690+DanielOnuwka@users.noreply.github.com>
2023-12-11 22:13:59 -0500

    Add files via upload

---
+++ YoutubeLesson.java
@@ -0,0 +1,14 @@
+// This here is an example of an in-line comment
+// I love Mr. Lake's coding class
+
+
+//below is a multi-line comment
+/*the in-line comments are only to be used on one line, but like the other comments, 
+they all come up in the same color which is green in  this case
+ */
+
+// next we have variables
+// in java, variables are represented using 'var', 'let', or 'cont'
+// 'var' allows the variable to be used throughout the program
+// "let" allows the variable to be used in the scope that is declared
+
