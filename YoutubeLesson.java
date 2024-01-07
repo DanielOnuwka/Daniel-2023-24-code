7b98182649638b4c71591bacf4088912a56fff76
DanielOnuwka <152315690+DanielOnuwka@users.noreply.github.com>
2024-01-07 00:51:28 -0500

    Add files via upload

--- YoutubeLesson.java
+++ YoutubeLesson.java
@@ -2,13 +2,111 @@
 // I love Mr. Lake's coding class
 
 
-//below is a multi-line comment
-/*the in-line comments are only to be used on one line, but like the other comments, 
+// below is a multi-line comment
+/* the in-line comments are only to be used on one line, but like the other comments, 
 they all come up in the same color which is green in  this case
  */
 
 // next we have variables
 // in java, variables are represented using 'var', 'let', or 'cont'
 // 'var' allows the variable to be used throughout the program
-// "let" allows the variable to be used in the scope that is declared
+// 'let' allows the variable to be used in the scope that is declared
+// 'const' a variable that can't be changed
 
+// you can declare and assign a variable 
+
+// declaring a variable
+var a;
+
+// assiging a variable
+var b = 2; 
+
+// the equal sign represents the assignment operator
+
+// console.log()
+// this allows the code to be printed in the terminal
+
+//playing with variables
+
+// these 3 variables will be initialized
+var a = 5;
+var b = 19;
+var c = "I am a ";
+
+// practice
+
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+
+
+// Casae sensitivity in Variables
+
+// Declarations
+var studyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// The declarations and the assignments won't be the same as what's in the assignments only because the capitalization is different.
+
+// Assignments
+studlyCAPVAR = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000; 
+
+// CamelCase ^
+/* This is where you capitalize the first letter of each word   */
+
+// Adding Numbers
+var sum = 10 + 10;
+
+// Subtracting Numbers
+var difference = 45 - 0; 
+
+// Multiplying Numbers
+var product = 8 * 0;
+
+// Dividing Numbers
+var quotient = 66 / 33;
+
+// incrementing Numbers (to add 1 to it)
+var myVar = 87;
+
+// Only change code below this line
+/* myVar = myVar + 1; 
+ * or myVar++;
+*/
+ // This sums up to 88
+
+ var myvar = 11;
+
+ // Only change code below this line
+ /* myVar = myVar - 1;
+  or myVar--;
+ */  
+
+ // Decimals
+ var ourDecimal = 5.7;
+
+ // Only change code below this line
+
+ var myDecimal = 0.009;
+
+// Multiplying Decimals
+
+var product = 2.0 * 2.5; 
+
+var qoutient = 4.4 / 2.0;
+
+// Declaring string variables 
+
+var firstName = 'Alan'
+
+ // Only change code below this line
+
+var myFirstName = "Beau";
+var myLastName = "Carnes";
+
+//How to escape literal quotes
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes"
